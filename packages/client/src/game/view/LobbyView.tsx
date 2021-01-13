46814146777d0b9d84cc85374d70217668b22bec
Samuel Sharp <samuelsharp4@gmail.com>
2021-01-12 17:58:34 -0800

    Clean lobbyview

--- packages/client/src/game/view/LobbyView.tsx
+++ packages/client/src/game/view/LobbyView.tsx
@@ -1,66 +1,20 @@
 import React from 'react';
-import {navigate} from '@reach/router';
 
 import { LobbyRenderState } from '../state/StateManager';
-import {Button, Center, Box, Space, Line} from '../../components';
-import Clipboard from 'react-clipboard.js';
-import {show_success_banner} from '../../util/banner';
+import { Button, Center, Box, Space } from '../../components';
 
 interface LobbyStateViewProps extends LobbyRenderState {}
 
-interface shareArgs {
-  title?: string;
-  text?: string;
-  url?: string
-}
-interface sharer {
-  share?: (x: shareArgs) => void;
-}
-
-function inviteFriends() {
-    (navigator as sharer).share!({
-      title: "Come play me!",
-      text: "You can use this link to join my lobby!",
-      url: window.location.href
-    });
-}
-
-const InviteButton = (_: {}) => {
-  if ((navigator as sharer).share) {
-    return <Button text={"Invite +"} onClick={inviteFriends}></Button>
-  }
-
-  return <Clipboard className="button" onSuccess={(_) => show_success_banner("Link copied to clipboard")} data-clipboard-text={window.location.href}>Copy Invite Code + </Clipboard>
-}
-
-interface listPlayer {
-  name: string;
-  key: string;
-}
 export const LobbyStateView = (props: LobbyStateViewProps) => {
-  const playerNames: listPlayer[] = [];
-
-  for(let id in props.players) {
-    playerNames.push({name: props.players[id].name, key: id})
-  }
-  const players = playerNames.map(player => {
-    return (<p key={player.key}>{player.name}</p>)
-  })
-
   const isHost = (props.sessionId in props.players) && props.players[props.sessionId].host
   return (
   <Center>
     <Space size='s'/>
     <Box>
-    <h1>Lobby</h1>
-    {isHost && <p>You're the host</p>}
+    <h1>Toybox 2020</h1>
     {isHost &&
-      <Button text="Start +" onClick={() => props.room.send("start")}></Button>}
-    {!isHost && <p>Waiting for host to start</p>}
-    <Line/>
-    {players}
-    <InviteButton/>
-    <Button text="Exit +" onClick={() => navigate("/")}></Button>
+      <Button text="Start Game" onClick={() => props.room.send("start")}></Button>}
+    {!isHost && <p>The game will start soon...</p>}
     </Box>
   </Center>)
 }
