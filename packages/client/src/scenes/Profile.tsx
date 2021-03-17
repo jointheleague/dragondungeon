be6c89994548d7d7afba5a922acc6b6349ee29c4
Samuel Sharp <samuelsharp4@gmail.com>
2021-03-16 18:34:00 -0700

    Profile

--- packages/client/src/scenes/Profile.tsx
+++ packages/client/src/scenes/Profile.tsx
@@ -6,6 +6,7 @@ import { navigate } from '@reach/router';
 import DOMPurify from 'dompurify';
 
 let firebaseApp: any;
+let data: any;
 
 try {
   firebaseApp = firebase.initializeApp({
@@ -19,8 +20,6 @@ try {
 } catch {
   window.location.reload();
 }
-
-
 
 const Profile = () => {
   const [profilePicture, setProfilePicture] = useState<string>('/icon.png');
@@ -47,8 +46,61 @@ const Profile = () => {
         <a href="/">Back</a>
         <h1>Profile</h1>
         <img src={profilePicture} alt="Profile" height="50px"/>
-        <h2>{currentUser.displayName || currentUser.phoneNumber || (currentUser.isAnonymous ? 'Anonymous' : '')}</h2>
-        <h3></h3>
+        <h1>{currentUser.displayName || currentUser.phoneNumber || (currentUser.isAnonymous ? 'Anonymous' : '')}</h1>
+        <h2>Stats</h2>
+        <h3><img src="/icon.png" height="20px" /> High Score: 0</h3>
+        <h3><img src="/icon.png" height="20px" /> Lifetime Coins: 0</h3>
+        <h3><img src="/icon.png" height="20px" /> Dragons Killed: 0</h3>
+        <h3><img src="/icon.png" height="20px" /> Badges Earned: 0</h3>
+        <h2>Abilities</h2>
+        <div style={{
+          backgroundColor: '#c60c30',
+          color: 'white',
+          borderRadius: '5px',
+          width: '100px',
+          padding: '10px',
+          cursor: 'pointer'
+        }}>
+          <img src="/icon.png" style={{ cursor: 'pointer' }} height="20px" />
+          <br />
+          <h4 style={{ cursor: 'pointer' }}>Fireballs</h4>
+        </div><br />
+        <div style={{
+          backgroundColor: '#00a1de',
+          color: 'white',
+          borderRadius: '5px',
+          width: '100px',
+          padding: '10px',
+          cursor: 'pointer'
+        }}>
+          <img src="/icon.png" style={{ cursor: 'pointer' }} height="20px" />
+          <br />
+          <h4 style={{ cursor: 'pointer' }}>Snowballs</h4>
+        </div><br />
+        <div style={{
+          backgroundColor: '#f9e300',
+          color: 'black',
+          borderRadius: '5px',
+          width: '100px',
+          padding: '10px',
+          cursor: 'pointer'
+        }}>
+          <img src="/icon.png" style={{ cursor: 'pointer' }} height="20px" />
+          <br />
+          <h4 style={{ cursor: 'pointer' }}>Lightning</h4>
+        </div><br />
+        <div style={{
+          backgroundColor: '#009b3a',
+          color: 'white',
+          borderRadius: '5px',
+          width: '100px',
+          padding: '10px',
+          cursor: 'pointer'
+        }}>
+          <img src="/icon.png" style={{ cursor: 'pointer' }} height="20px" />
+          <br />
+          <h4 style={{ cursor: 'pointer' }}>Poison</h4>
+        </div><br />
       </Center>
     </>
   );
