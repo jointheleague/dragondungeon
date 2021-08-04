a27e2214285c25f5c8e4ab9356cb672729655547
keithagroves <keith.groves@jointheleague.org>
2021-08-03 17:35:44 -0700

    change dragonbot movements

--- packages/server/src/rooms/GameRoom.ts
+++ packages/server/src/rooms/GameRoom.ts
@@ -1,4 +1,5 @@
 import {
+	getDistance,
 	getRandomInt
 } from '@dragoncoin/common/build/maths';
 
@@ -11,7 +12,7 @@ import {
 	GameState,
 	Player,
 	IInputs,
-	Coin, 
+	Coin,
 	CoinJar,
 	Bar,
 	Maths,
@@ -24,12 +25,13 @@ import { v4 } from "uuid";
 
 const admin_sdk_key = require('../../top_secret/adminsdk.json');
 const botnames = require('./botnames.json');
+const MAX_COINS = 10;
 
 admin.initializeApp({
-  credential: admin.credential.cert(admin_sdk_key)
+	credential: admin.credential.cert(admin_sdk_key)
 });
 
-export class GameRoom extends Room < GameState > {
+export class GameRoom extends Room<GameState> {
 	counter = 0;
 	botTimeout = 0;
 	maxClients: 10
@@ -41,38 +43,38 @@ export class GameRoom extends Room < GameState > {
 		this.state.countdown = new Countdown(5, 0);
 	}
 
-	async onJoin(client: Client, options: {token: string}, _2: any) {
+	async onJoin(client: Client, options: { token: string }, _2: any) {
 		const user = await admin.auth().verifyIdToken(options.token);
 		var s1 = "";
 		var s2 = "";
-		switch(Math.floor(Math.random()*5)){
-            case 0:
-             	s1 = "fire";
-             	break;
-            case 1:
-             	s1 = "ice";
-              	break;
-            case 2:
-        		s1 = "poison";
-            	break;
-            case 3:
-            	s1 = "mud";
-            	break;
+		switch (Math.floor(Math.random() * 5)) {
+			case 0:
+				s1 = "fire";
+				break;
+			case 1:
+				s1 = "ice";
+				break;
+			case 2:
+				s1 = "poison";
+				break;
+			case 3:
+				s1 = "mud";
+				break;
 			case 4:
 				s1 = "electricity";
 				break;
-        }
-		switch(Math.floor(Math.random()*3)){
-        	case 0:
-           		s2 = "normal";
-         		break;
-          	case 1:
-            	s2 = "light";
-            	break;
-        	case 2:
-        		s2 = "gold";
-            	break;
-        }
+		}
+		switch (Math.floor(Math.random() * 3)) {
+			case 0:
+				s2 = "normal";
+				break;
+			case 1:
+				s2 = "light";
+				break;
+			case 2:
+				s2 = "gold";
+				break;
+		}
 		this.state.players[client.id] = new Player(s1, s2);
 		if (user.name == null) {
 			const adjectives = require('../../wordlists/adjectives.json');
@@ -105,21 +107,21 @@ export class GameRoom extends Room < GameState > {
 			this.clock.tick();
 			this.tick();
 		}, 1000 / 60);
-		
+
 	}
-	
+
 	cancelGameLoop() {
 		this.clock.clear()
 	}
 
-	spawnCoin(){
+	spawnCoin() {
 		var num = Math.random();
 		var size = 20;
-		if(num >= .75){
+		if (num >= .75) {
 			size += 5;
-			if(num >= .95){
+			if (num >= .95) {
 				size += 5;
-				if(num >= .995){
+				if (num >= .995) {
 					size = 100;
 				}
 			}
@@ -129,12 +131,12 @@ export class GameRoom extends Room < GameState > {
 		do {
 			newX = Math.random() * 2000;
 			newY = Math.random() * 2000;
-		}while(Maths.checkWalls(newX, newY, size) || (newX > 700 && newY > 700 && newX < 1300 && newY < 1300))
+		} while (Maths.checkWalls(newX, newY, size) || (newX > 700 && newY > 700 && newX < 1300 && newY < 1300))
 		this.state.coins.set(v4(), new Coin(this.state.coins.size, newX, newY, size));
-	
+
 	}
 
-	createCoin(x: number, y: number){
+	createCoin(x: number, y: number) {
 		var rand;
 		var newX;
 		var newY;
@@ -142,8 +144,24 @@ export class GameRoom extends Room < GameState > {
 			rand = getRandomInt(0, 62) / 10;
 			newX = x + 100 * Math.cos(rand);
 			newY = y + 100 * Math.sin(rand);
-		}while(Maths.checkWalls(newX, newY, 20))
+		} while (Maths.checkWalls(newX, newY, 20))
 		this.state.coins.set(v4(), new Coin(this.state.coins.size, newX, newY, 20));
+	}
+	moveBot(id, botDir) {
+		let yMove = Math.random() > 0.5;
+		let xMove = Math.random() > 0.5
+		let left = xMove;
+		let right = !xMove;
+		let up = yMove;
+		let down = !yMove;
+
+		this.state.players[id].inputs({
+			up: up,
+			down: down,
+			left: left,
+			right: right,
+		});
+
 	}
 
 	tick() {
@@ -183,46 +201,39 @@ export class GameRoom extends Room < GameState > {
 					break;
 				case 4:
 					bot.onlineName = botNameFirst + "1";
-					break; 
+					break;
 			}
 			this.state.players.set(v4(), bot);
 		}
-		
+
 
 		for (let id of this.state.players.keys()) {
 			if (this.state.players[id].isBot && this.botTimeout == 0) {
+				const bot = this.state.players[id];
+				const jar = this.state.coinJar;
+				const range = 50;
 				let botDir = Math.floor(Math.random() * 4);
-				switch(botDir) {
-					case 0:
-						this.state.players[id].inputs({
-							up: true,
-							down: false,
-						});
-						break;
-					case 1:
-						this.state.players[id].inputs({
-							up: false,
-							down: true,
-							space: true,
-						});
-						break;
-					case 2:
-						this.state.players[id].inputs({
-							left: true,
-							right: false,
-						});
-						break;
-					case 3:
-						this.state.players[id].inputs({
-							left: false,
-							right: true,
-						});
-						break;
+				if (bot.coins > MAX_COINS / 4) {
+					if (getDistance(bot.x, bot.y, jar.x, jar.y) < 450) {
+						if (bot.x > jar.x + range) {
+							botDir = 2;
+						} else if (bot.x < jar.x - range) {
+							botDir = 3;
+						} else if (bot.y < jar.y + range) {
+							botDir = 1;
+						}
+						else if (bot.y > jar.y + range) {
+							botDir = 0;
+						}
+
+
 					}
 				}
-			
+				this.moveBot(id, botDir);
+			}
+
 			if (this.botTimeout == 0) {
-				this.botTimeout = Math.floor(Math.random() * 20) + 1;
+				this.botTimeout = Math.floor(Math.random() * 50) + 1;
 			}
 
 			this.botTimeout--;
@@ -233,7 +244,7 @@ export class GameRoom extends Room < GameState > {
 				for (let i = 0; i < this.state.players[id2].fireballs.length; i++) {
 					if (id != id2) {
 						if (this.state.players[id2].fireballs[i].checkHit(this.state.players[id].x, this.state.players[id].y)) {
-						    var fireBall = this.state.players[id2].fireballs[i];
+							var fireBall = this.state.players[id2].fireballs[i];
 							const coinChance = .2; // the possibility of removing a coin on collision with a fireball, this is done to spread out the coins more
 							const lifetimeRemove = 1; // the lifetime decreace of the fireball for every coin it removes from a dragon (as if  it is heavier)
 
@@ -242,37 +253,37 @@ export class GameRoom extends Room < GameState > {
 							const newX = oldX + (fireBall.speed * Math.cos(fireBall.angle - Math.PI));
 							const newY = oldY + (fireBall.speed * Math.sin(fireBall.angle - Math.PI));
 
-							if(!Maths.checkWalls(oldX, newY, 45)){
+							if (!Maths.checkWalls(oldX, newY, 45)) {
 								this.state.players[id].y = newY;
 							}
-							if(!Maths.checkWalls(newX, oldY, 45)){
+							if (!Maths.checkWalls(newX, oldY, 45)) {
 								this.state.players[id].x = newX;
 							}
 							//console.log(this.state.players[id].x + "    " + this.state.players[id].y)
 							if (this.state.players[id].coins > 0 && Math.random() < coinChance) {
-								this.state.players[id].coins --;
+								this.state.players[id].coins--;
 								fireBall.lifetime -= lifetimeRemove;
-								if(fireBall.type == "poison" && this.state.players[id2].coins < 10){
-									this.state.players[id2].coins ++;
-								}else{
-									this.createCoin(this.state.players[id].x,this.state.players[id].y);
+								if (fireBall.type == "poison" && this.state.players[id2].coins < 10) {
+									this.state.players[id2].coins++;
+								} else {
+									this.createCoin(this.state.players[id].x, this.state.players[id].y);
 								}
 							}
 
 
 							switch (fireBall.type) {
 								case "electricity":
-									if(this.state.players[id2].fireballs.length < 10 &&  Math.random() > .9){
+									if (this.state.players[id2].fireballs.length < 10 && Math.random() > .9) {
 										const angle = Math.random() * 6.28;
 										const newX = this.state.players[id].x + 50 * Math.cos(angle);
 										const newY = this.state.players[id].y + 50 * Math.sin(angle);
-										if(!Maths.checkWalls(newX, newY, 22.5)){
+										if (!Maths.checkWalls(newX, newY, 22.5)) {
 											this.state.players[id2].fireballs.push(new Fireball(newX, newY, angle + Math.PI, 7, "electricity", 20));
-											
+
 										}
 									}
 									break;
-								case "mud":	
+								case "mud":
 									fireBall.width += 1;
 									fireBall.height += 1.87;
 									//fireBall.speed += .05;
@@ -284,19 +295,19 @@ export class GameRoom extends Room < GameState > {
 						}
 					}
 				}
-				
-				if(this.state.coinJar.checkHit(this.state.players[id].x, this.state.players[id].y)){
+
+				if (this.state.coinJar.checkHit(this.state.players[id].x, this.state.players[id].y)) {
 					// when a player has collided with the coinjar
 					this.state.players[id].score += this.state.players[id].coins;// add coins to players score
 					this.state.players[id].coins = 0;// remove coins
 				}
 
-				for(let cid of this.state.coins.keys()){
+				for (let cid of this.state.coins.keys()) {
 					if (this.state.coins[cid].checkHit(this.state.players[id].x, this.state.players[id].y) == true && this.state.players[id].coins < 10) {
 						var coins = this.state.players[id].coins;
-						switch(this.state.coins[cid].getSize()){
+						switch (this.state.coins[cid].getSize()) {
 							case (20):
-								coins ++;
+								coins++;
 								break;
 							case (25):
 								coins += 2;
@@ -308,23 +319,23 @@ export class GameRoom extends Room < GameState > {
 								this.state.players[id].score += 50;
 								break;
 						}
-						this.state.players[id].coins = Math.min(coins,10);
+						this.state.players[id].coins = Math.min(coins, 10);
 						this.state.coins.delete(cid);
 					}
 				}
 
-				if(this.state.players[id].x < 0){
+				if (this.state.players[id].x < 0) {
 					this.state.players[id].x = 0;
-				} else if(this.state.players[id].x > 2000){
+				} else if (this.state.players[id].x > 2000) {
 					this.state.players[id].x = 2000;
 				}
 
-				if(this.state.players[id].y < 0){
+				if (this.state.players[id].y < 0) {
 					this.state.players[id].y = 0;
-				} else if(this.state.players[id].y > 2000){
+				} else if (this.state.players[id].y > 2000) {
 					this.state.players[id].y = 2000;
 				}
-				
+
 			}
 			// console.log(id + "  " + this.state.players[id].score);
 		}
