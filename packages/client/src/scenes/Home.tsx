dd05d6bfb6cb5fe083f25bc30fa7bda7b49681e1
Samuel Sharp <samuelsharp4@gmail.com>
2021-08-14 09:06:09 -0700

    Hotfix: modified loading animation to be slightly less painful

--- packages/client/src/scenes/Home.tsx
+++ packages/client/src/scenes/Home.tsx
@@ -4,7 +4,6 @@ import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import { getFirestore, getDoc, doc } from 'firebase/firestore/lite';
 import { navigate } from '@reach/router';
 import { show_error_banner } from 'util/banner';
-import useSound from 'use-sound';
 import  * as Mousetrap from "mousetrap";
 
 const db = getFirestore();
@@ -28,45 +27,22 @@ const resume = () => {
   });
 }
 
-Mousetrap.bind('enter', resume);
-Mousetrap.bind('shift+enter', () => show_error_banner('Coming Soon'));
-Mousetrap.bind('alt+enter', () => navigate('/mydragon'));
-
 const Home = (props: any) => {
   useEffect(() => {
+    Mousetrap.bind('enter', resume);
+    Mousetrap.bind('shift+enter', () => show_error_banner('Coming Soon'));
+    Mousetrap.bind('alt+enter', () => navigate('/mydragon'));
+
     const music = new Audio('/music/startscreen.mp3');
     music.play();
 
     setTimeout(() => {
-      (document.querySelector('#load1') as any).style.display = 'block';
-    }, 1000)
-
-    setTimeout(() => {
-      (document.querySelector('#load2') as any).style.display = 'block';
-    }, 2000)
-
-    setTimeout(() => {
-      (document.querySelector('#load3') as any).style.display = 'block';
-    }, 3000)
-
-    setTimeout(() => {
-      (document.querySelector('#load4') as any).style.display = 'block';
-    }, 4000)
-
-    setTimeout(() => {
-      (document.querySelector('#load5') as any).style.display = 'block';
+      (document.querySelector('#league-logo') as any).style.display = 'none';
+      (document.querySelector('#dragoncoin-logo') as any).style.display = 'block';
     }, 5000)
 
     setTimeout(() => {
-      (document.querySelector('#load6') as any).style.display = 'block';
-    }, 8000)
-
-    setTimeout(() => {
-      (document.querySelector('#load7') as any).style.display = 'block';
-    }, 10000)
-
-    setTimeout(() => {
-      (document.querySelector('#league-logo') as any).style.display = 'none';
+      (document.querySelector('#logo-screen') as any).style.display = 'none';
     }, 11000)
 
     return () => {
@@ -75,7 +51,7 @@ const Home = (props: any) => {
   }, []);
   return (
     <>
-      <div id="league-logo" style={{ 
+      <div id="logo-screen" style={{ 
         position: 'fixed',
         top: 0,
         left: 0,
@@ -83,30 +59,20 @@ const Home = (props: any) => {
         height: '100vh',
         backgroundColor: 'black',
        }}>
-         <img src="/jtl.png" style={{
+         <img id="league-logo" src="/jtl.png" style={{
            position: 'absolute',
            top: '30%',
            left: '30%',
            height: '150px'
          }} />
-         <div style={{
+         <h1 id="dragoncoin-logo" style={{
            position: 'absolute',
-           top: '60%',
-           left: '30%',
-           height: '150px'
-         }}>
-           <table>
-             <tr>
-               <td><img src="/fireball.png" id="load1" style={{ display: 'none', height: '80px', imageRendering: 'pixelated', padding: '15px' }} /></td>
-               <td><img src="/iceball.png" id="load2" style={{ display: 'none', height: '80px', imageRendering: 'pixelated', padding: '15px' }} /></td>
-               <td><img src="/electricball.png" id="load3" style={{ display: 'none', height: '80px', imageRendering: 'pixelated', padding: '15px' }} /></td>
-               <td><img src="/mudball.png" id="load4" style={{ display: 'none', height: '80px', imageRendering: 'pixelated', padding: '15px' }} /></td>
-               <td><img src="/poisonball.png" id="load5" style={{ display: 'none', height: '80px', imageRendering: 'pixelated', padding: '15px' }} /></td>
-               <td><img src="/icon.png" id="load6" style={{ display: 'none', height: '80px', imageRendering: 'pixelated', padding: '15px' }} /></td>
-               <td><img src="/basicDragon.png" id="load7" style={{ display: 'none', height: '80px', imageRendering: 'pixelated', padding: '15px' }} /></td>
-             </tr>
-           </table>
-         </div>
+           top: '35%',
+           left: '25%',
+           display: 'none',
+           fontSize: '40pt',
+           color: '#c60c30'
+         }}>DragonCoin Alpha</h1>
       </div>
       <div id="page">
         <div id="heroContent" style={{ float: 'right', imageRendering: 'pixelated', padding: '20px' }}>
