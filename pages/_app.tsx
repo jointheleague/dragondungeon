e150089ef57548ac5dac144a36974af0efac8673
Samuel Sharp <samuelsharp4@gmail.com>
2021-11-17 02:40:37 +0000

    Add firebase initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,28 @@
+import Head from 'next/head'
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from 'firebase/auth'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function DragonDungeon({ Component, pageProps }) {
+  initializeApp({
+    apiKey: "AIzaSyCRClPzTZnRSg_fAap6ENnAkxUBQKJGk5w",
+    authDomain: "leaguedragoncoin.firebaseapp.com",
+    projectId: "leaguedragoncoin",
+    storageBucket: "leaguedragoncoin.appspot.com",
+    messagingSenderId: "320692217416",
+    appId: "1:320692217416:web:f9cd0efdc04445865e9a7d"
+  })
+
+  let auth = getAuth()
+  
+  return <>
+    <Head>
+      <title>Dragon Dungeon</title>
+      <link rel="icon" href="/img/game/coinJar.png" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
-export default MyApp
+export default DragonDungeon
